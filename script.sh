41f0928f9d72b45af511597edb16807ad490688a
GouthamGajam <gouthamgajam@gmail.com>
2024-07-05 10:51:36 +0530

    setup done

---
+++ script.sh
@@ -0,0 +1,14 @@
+mkdir config controllers models utils 
+touch .gitignore
+echo "node_modules" > .gitignore
+echo "config" >> .gitignore
+cd config
+touch default.json
+cd ..
+cd utils
+touch dbconnect.js
+cd ..
+npm init -y
+touch app.js
+npm install express config axios mongoose nodemon
+ 
